a18209635b0116cd302d5404617b005f58517880
Jon-Morten Kristiansen <jon.morten.kristiansen@gmail.com>
2014-03-11 22:11:07 +0100

    Setup: Switch from Rake to bash

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ensure_link()
+{
+	test -L "$HOME/$2" || ln -s "$DOTFILES_DIR/files/$1" "$HOME/$2"
+}
+
+
+# bash
+ensure_link 'bash_profile'  '.bash_profile'
+ensure_link 'bashrc'        '.bashrc'
+ensure_link 'profile'       '.profile'
+
+# git
+ensure_link 'gitconfig'     '.gitconfig'
+ensure_link 'gitignore'     '.gitignore'
+
+# vim
+ensure_link 'vimrc'         '.vimrc'
