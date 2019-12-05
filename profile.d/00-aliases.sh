6f74b7591cb6824d743089da86b443ac66845664
Jon-Morten Kristiansen <jon-morten@urbansharing.com>
2019-12-05 09:50:47 +0100

    Add some common aliases

--- profile.d/00-aliases.sh
+++ profile.d/00-aliases.sh
@@ -2,8 +2,16 @@ alias st='open -a "sublime text"'
 alias sc='open -a "visual studio code"'
 
 alias g='git'
+alias ya='yarn'
 
 alias v='vagrant'
 alias vh='vagrant halt'
 alias vs='vagrant suspend'
 alias vu='vagrant up'
+
+alias ll='ls -alF'
+alias ..='cd ../'
+
+alias givkey="cat ~/.ssh/id_rsa.pub | tr -d '\n' | pbcopy"
+
+alias python2="python2.7"
